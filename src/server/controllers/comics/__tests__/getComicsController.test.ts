d345f170e3eb70430595e50c72f477db837ded9d
Gloria Talavera <gloria.tare@gmail.com>
2023-11-30 11:39:31 +0100

    Add limit and sort to getComics controller

--- src/server/controllers/comics/__tests__/getComicsController.test.ts
+++ src/server/controllers/comics/__tests__/getComicsController.test.ts
@@ -4,12 +4,15 @@ import Comic from "../../../../database/models/Comic.js";
 import CustomError from "../../../../CustomError/CustomError.js";
 import { getComics } from "../comicsControllers.js";
 import { comicsMock } from "../../../../mocks/comicsMock.js";
+import { type AuthRequest } from "../../../middlewares/auth/types.js";
 
 beforeEach(() => {
   jest.clearAllMocks();
 });
 
-const req: Partial<Request> = {};
+const req: Partial<Request> = {
+  query: { limit: "10" },
+};
 
 const res: Partial<Response> = {
   status: jest.fn().mockReturnThis(),
@@ -20,23 +23,26 @@ const next: NextFunction = jest.fn();
 
 describe("Given a getComics controller", () => {
   describe("When it receives a response", () => {
+    Comic.find = jest.fn().mockReturnValue({
+      sort: jest.fn().mockReturnThis(),
+      limit: jest.fn().mockReturnThis(),
+      exec: jest.fn().mockResolvedValue(comicsMock),
+    });
+
     test("Then it should call its method status with code 200", async () => {
       const expectedStatusCode = 200;
 
-      Comic.find = jest.fn().mockReturnValue({
-        limit: jest.fn().mockReturnThis(),
-        exec: jest.fn().mockResolvedValue(comicsMock),
-      });
-
-      await getComics(req as Request, res as Response, next);
+      await getComics(req as AuthRequest, res as Response, next);
 
       expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
     });
 
     test("Then it should call its method json with 'Maus' and 'Persepolis'", async () => {
-      await getComics(req as Request, res as Response, next);
+      await getComics(req as AuthRequest, res as Response, next);
 
-      expect(res.json).toHaveBeenCalledWith({ comics: comicsMock });
+      expect(res.json).toHaveBeenCalledWith({
+        comics: comicsMock,
+      });
     });
   });
 
@@ -50,10 +56,11 @@ describe("Given a getComics controller", () => {
 
       Comic.find = jest.fn().mockReturnValue({
         limit: jest.fn().mockReturnThis(),
+        sort: jest.fn().mockReturnThis(),
         exec: jest.fn().mockRejectedValue(expectedError),
       });
 
-      await getComics(req as Request, res as Response, next);
+      await getComics(req as AuthRequest, res as Response, next);
 
       expect(next).toHaveBeenCalledWith(expectedError);
     });
